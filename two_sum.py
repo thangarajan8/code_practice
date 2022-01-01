a35ed34110d4c281b7fd64696b1385c7fe26765f
itsthanga <thangam.rajan8@gmail.com>
2022-01-01 11:48:44 +0530

    01_01_2022

---
+++ two_sum.py
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/two-sum/
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                if nums[i]+nums[j] == target and i!=j:
+                    return [i,j]
